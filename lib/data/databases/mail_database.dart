3678880e1e311e79a4d4f366a492c2ae0a29d3e8
Eiji Otieno <eijiotieno.official.mail@gmail.com>
2024-03-08 12:14:29 +0300

    get all

--- lib/data/databases/mail_database.dart
+++ lib/data/databases/mail_database.dart
@@ -1,7 +1,26 @@
 import 'package:gmail_ui/data/models/mail.dart';
+import 'package:gmail_ui/services/generate_attachments.dart';
+import 'package:gmail_ui/services/generate_body.dart';
+import 'package:gmail_ui/services/generate_recipients.dart';
+import 'package:gmail_ui/services/generate_subjects.dart';
 
 class MailDatabase {
   static List<Mail> data = [];
 
-  static Future<void> getAll() async {}
+  static Future<void> getAll(int numberOfMails) async {
+    data.clear();
+
+    for (int i = 1; i <= numberOfMails; i++) {
+      Mail mail = Mail(
+        id: i.toString(),
+        sender: 'me@example.com',
+        subject: generateSubject(1),
+        body: generateBody(3),
+        recipients: generateRecipients(i),
+        timestamp: DateTime.now().subtract(Duration(days: i)),
+        attachments: generateAttachments(2),
+      );
+      data.add(mail);
+    }
+  }
 }
