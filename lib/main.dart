144ffc898616b3778b474d126ec3f9a378ef6762
Eiji Otieno <eijiotieno.official.mail@gmail.com>
2024-03-08 12:29:17 +0300

    dynamic color

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,12 +10,24 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return DynamicColorBuilder(
+      builder: (lightDynamic, darkDynamic) {
+        return MaterialApp(
+          theme: ThemeData(
+            colorScheme: lightDynamic,
+            useMaterial3: true,
+          ),
+          darkTheme: ThemeData(
+            colorScheme: darkDynamic,
+            useMaterial3: true,
+          ),
+          home: Scaffold(
+            body: Center(
+              child: Text('Hello World!'),
+            ),
+          ),
+        );
+      },
     );
   }
 }
