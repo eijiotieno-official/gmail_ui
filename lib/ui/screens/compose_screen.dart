dd91c950256164bfab3a20729c6d8b1fdc299424
Eiji Otieno <eijiotieno.official.mail@gmail.com>
2024-05-20 14:14:14 +0300

    compose app bar

--- lib/ui/screens/compose_screen.dart
+++ lib/ui/screens/compose_screen.dart
@@ -10,6 +10,62 @@ class ComposeScreen extends StatefulWidget {
 class _ComposeScreenState extends State<ComposeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.attach_file_rounded,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.send_rounded,
+            ),
+          ),
+          PopupMenuButton(
+            itemBuilder: (context) {
+              return <PopupMenuEntry>[
+                PopupMenuItem(
+                  onTap: () {},
+                  child: const Text("Schedule send"),
+                ),
+                PopupMenuItem(
+                  onTap: () {},
+                  child: const Text("Add from Contacts"),
+                ),
+                PopupMenuItem(
+                  onTap: () {},
+                  child: const Text("Confidential mode"),
+                ),
+                PopupMenuItem(
+                  onTap: () {},
+                  child: const Text("Save draft"),
+                ),
+                PopupMenuItem(
+                  onTap: () {},
+                  child: const Text("Discard"),
+                ),
+                PopupMenuItem(
+                  onTap: () {},
+                  child: const Text("Settings"),
+                ),
+                PopupMenuItem(
+                  onTap: () {},
+                  child: const Text("Help and feedback"),
+                ),
+              ];
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
   }
 }
