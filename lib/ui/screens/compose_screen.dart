70ba9b8621a2d5861e92734f4fbb46103122ca89
Eiji Otieno <eijiotieno.official.mail@gmail.com>
2024-05-20 14:05:03 +0300

    compose screen

---
+++ lib/ui/screens/compose_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ComposeScreen extends StatefulWidget {
+  const ComposeScreen({super.key});
+
+  @override
+  State<ComposeScreen> createState() => _ComposeScreenState();
+}
+
+class _ComposeScreenState extends State<ComposeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
